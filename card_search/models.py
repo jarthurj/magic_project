1646dce1174d67759dfe3f79854578d9d9a78d62
Johnny <jaoldaker1@gmail.com>
2021-07-30 17:21:28 -0400

    database complete

--- card_search/models.py
+++ card_search/models.py
@@ -19,6 +19,19 @@ class Rarity(models.Model):
 class Set_name(models.Model):
 	set_name = models.CharField(max_length=50)
 
+class Layout(models.Model):
+	layout= models.CharField(max_length=45)
+
+class Mana_cost(models.Model):
+	mana_cost= models.CharField(max_length=45)
+class Cmc(models.Model):
+	cmc = models.FloatField()
+
+class Legalities(models.Model):
+	name = models.CharField(max_length=45)
+
+
+
 class Card(models.Model):
 	name = models.CharField(max_length=150)
 	flavor_text = models.CharField(max_length=450, null=True)
@@ -26,6 +39,7 @@ class Card(models.Model):
 	small = models.URLField()
 	normal = models.URLField()
 	large = models.URLField()
+	type_line = models.CharField(max_length=100, null=True)
 
 	artist = models.ForeignKey(Artist, related_name="cards", on_delete=models.CASCADE)
 	digital = models.ForeignKey(Digital, related_name="cards", on_delete=models.CASCADE)
@@ -35,25 +49,28 @@ class Card(models.Model):
 	power = models.ForeignKey(Power, related_name="cards", on_delete=models.CASCADE, null=True)
 	toughness = models.ForeignKey(Toughness,related_name="cards", on_delete=models.CASCADE, null=True)
 
-	# legalities
-	# games
-	# 	mtgo
-	# 	paper
-	# 	arena	
+	layout = models.ForeignKey(Layout, related_name="cards", on_delete=models.CASCADE, null=True)
+	mana_cost = models.ForeignKey(Mana_cost, related_name="cards", on_delete=models.CASCADE,null=True)
+	cmc = models.ForeignKey(Cmc, related_name="cards", on_delete=models.CASCADE,null=True)
 
+class Legal(models.Model):
+	name = models.ForeignKey(Legalities, related_name="legals", on_delete=models.CASCADE)
+	legal = models.BooleanField(null=True)
+	card = models.ForeignKey(Card, related_name="legals", on_delete=models.CASCADE, null=True)
 
-	# mana_cost
-	# cmc
-	# type_line
-	# layout
-
-	# keywords
 class Colors(models.Model):
 	color = models.CharField(max_length=1)
 	cards = models.ManyToManyField(Card, related_name="colors")
 class Color_identity(models.Model):
 	color_iden = models.CharField(max_length=1)
 	cards = models.ManyToManyField(Card, related_name="color_idens")
+class Keyword(models.Model):
+	keyword = models.CharField(max_length=45)
+	cards = models.ManyToManyField(Card, related_name="keywords")
+
+class Card_type(models.Model):
+	card_type= models.CharField(max_length=45)
+	cards = models.ManyToManyField(Card,related_name="card_types")
 #Powers to clean up
 # '∞','?', '*²', '*', 
 
