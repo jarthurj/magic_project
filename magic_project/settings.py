b5067752329b7643356738d368920f635ee465ac
jarthurj <jaoldaker1@gmail.com>
2022-06-04 09:11:35 -0400

    sdfklasdjf;lkasdjf
    joiefjweiofjoeifjwoiefj89"
    
    1#

--- magic_project/settings.py
+++ magic_project/settings.py
@@ -17,7 +17,6 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -123,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
